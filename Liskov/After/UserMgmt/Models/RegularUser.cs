eb1b0507d1e0633b6ea7a60a1b6beb62b7bfce1e
Dan Patrascu-Baba <dan@danpatrascu.com>
2022-03-09 11:56:38 +0200

    Liskov UserMgmt setup

---
+++ Liskov/After/UserMgmt/Models/RegularUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Before.UserMgmt.Models;
+
+public class RegularUser : User
+{
+    public RegularUser(string username, string password) : base(username, password)
+    {
+        
+    }
+}
---
+++ Liskov/Before/UserMgmt/Models/Administrator.cs
@@ -0,0 +1,23 @@
+﻿namespace Before.UserMgmt.Models;
+
+public class Administrator : User
+{
+    public Administrator(string username, string password) : base(username, password)
+    {
+        
+    }
+    public override bool CheckPasswordValid()
+    {
+        return Password.Length > 8 && ContainsDigit(Password);
+    }
+
+    private bool ContainsDigit(string stringToCheck)
+    {
+        foreach (var ch in stringToCheck)
+        {
+            if (Char.IsNumber(ch)) return true;
+        }
+
+        return false;
+    }
+}
